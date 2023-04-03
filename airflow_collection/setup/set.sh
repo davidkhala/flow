846dde544bd89a4d6e1727d524a5d68202a4007c
David Liu <david-khala@hotmail.com>
2023-04-03 11:06:23 +0800

    Update set.sh

--- airflow_collection/setup/set.sh
+++ airflow_collection/setup/set.sh
@@ -1,9 +1,12 @@
 clusterName=${cluster:-airflow-cluster-name}
 region=${region:-asia-east2}
+k8sNamespace=${namespace:-airflow}
+
 
 up(){
   gcloud container clusters create ${clusterName} --machine-type n1-standard-4 --num-nodes 1 --region $region
   gcloud container clusters get-credentials ${clusterName} --region $region
+  kubectl create namespace ${k8sNamespace}
 
 }
 down(){
