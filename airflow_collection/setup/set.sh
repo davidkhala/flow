61d35a66bff49fefc12b4621987674048608cc73
David Liu <david-khala@hotmail.com>
2023-04-03 14:12:59 +0800

    Update set.sh
    close #2

--- airflow_collection/setup/set.sh
+++ airflow_collection/setup/set.sh
@@ -5,10 +5,12 @@ helmName=${name:-airflow}
 
 
 up() {
+  # cluster create step can last for minutes
   gcloud container clusters create ${clusterName} --machine-type n1-standard-4 --num-nodes 1 --region $region # TODO consider k8s autoscale here.
   gcloud container clusters get-credentials ${clusterName} --region $region
   kubectl create namespace ${k8sNamespace}
   helm repo add apache-airflow https://airflow.apache.org
+  # helm install/upgrade step can last for minutes
   helm upgrade --install ${helmName} apache-airflow/airflow -n ${k8sNamespace} --debug
   # By default, the Helm chart is configured to use the `CeleryExecutor` which is why there is a `airflow-worker` and `airflow-redis` service. 
     
