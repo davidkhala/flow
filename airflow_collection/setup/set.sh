e92de021ccd2ead0aafec49a16f21e37c925ca0e
David Liu <david-khala@hotmail.com>
2023-04-03 11:33:30 +0800

    Update set.sh

--- airflow_collection/setup/set.sh
+++ airflow_collection/setup/set.sh
@@ -4,10 +4,10 @@ k8sNamespace=${namespace:-airflow}
 
 
 up(){
-  gcloud container clusters create ${clusterName} --machine-type n1-standard-4 --num-nodes 1 --region $region
+  gcloud container clusters create ${clusterName} --machine-type n1-standard-4 --num-nodes 1 --region $region # TODO consider k8s autoscale here.
   gcloud container clusters get-credentials ${clusterName} --region $region
   kubectl create namespace ${k8sNamespace}
-
+  helm repo add apache-airflow https://airflow.apache.org
 }
 down(){
   
