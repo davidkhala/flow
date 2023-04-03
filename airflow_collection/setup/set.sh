ac32d9bb494178efeb34e5141ba876c89d7b3980
David Liu <david-khala@hotmail.com>
2023-04-03 19:08:42 +0800

    [WIP] setLocal port-forward success TODO: use variable inline

--- airflow_collection/setup/set.sh
+++ airflow_collection/setup/set.sh
@@ -1,48 +1,57 @@
+set -e
 clusterName=${cluster:-airflow-cluster-name}
 region=${region:-asia-east2}
 k8sNamespace=${namespace:-airflow}
 helmName=${name:-airflow}
 
-
-up() {
+upGKE() {
   # cluster create step can last for minutes
   gcloud container clusters create ${clusterName} --machine-type n1-standard-4 --num-nodes 1 --region $region # TODO consider k8s autoscale here.
   gcloud container clusters get-credentials ${clusterName} --region $region
   kubectl create namespace ${k8sNamespace}
-  
-  helm repo add apache-airflow https://airflow.apache.org
+
+}
+upAirflow() {
   helm repo update
+  helm repo add apache-airflow https://airflow.apache.org
   # helm install/upgrade step can last for minutes
   helm upgrade --install ${helmName} apache-airflow/airflow -n ${k8sNamespace} --debug
-  # post-install, please wait until airflow-worker is up and running
-  
-  
+  # or use airflowHelmExamles()
+
+}
+upLocalhost() {
+  upGKE
+  upAirflow
+  setLocal
+}
+update() {
   kubectl create secret generic airflow-postgresql -n ${k8sNamespace} --from-literal=password='postgres' --dry-run=client -o yaml | kubectl apply -f -
-  
-  helm show values apache-airflow/airflow > values.yaml
-  # TODO manual edit to values.yaml 
+
+  helm show values apache-airflow/airflow >values.yaml
+  # TODO manual edit to values.yaml
   # - change `.executor` from `CeleryExecutor` to `LocalExecutor`
   # - change `.webserver.service.type` from `ClusterIP` to `LoadBalancer`
-  
+
   helm upgrade --install ${helmName} apache-airflow/airflow -n ${k8sNamespace} -f values.yaml --debug
-  
 }
 down() {
-  helm delete ${helmName} --namespace ${k8sNamespace}
   kubectl delete namespace ${k8sNamespace}
   gcloud container clusters delete ${clusterName} --region $region -q
 
 }
-setLocal(){
+downAirflow(){
+  helm delete ${helmName} --namespace ${k8sNamespace}
+}
+setLocal() {
   # http://localhost:8080/
   kubectl port-forward svc/${helmName}-webserver 8080:8080 --namespace ${k8sNamespace}
 }
 
-status(){
+status() {
   kubectl get pods --namespace ${k8sNamespace}
   helm list --namespace ${k8sNamespace}
 }
-sampleInstall(){
+airflowHelmExamles() {
   helm upgrade --install ${helmName} apache-airflow/airflow --namespace ${k8sNamespace} --set-string "env[0].name=AIRFLOW__CORE__LOAD_EXAMPLES" --set-string "env[0].value=True"
 }
 
